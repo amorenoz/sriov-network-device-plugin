8a5360bb41e7dd13329c5ea6e75097a243c6b0b7
Adrian Moreno <amorenoz@redhat.com>
2020-04-22 10:38:43 +0200

    [uvdpa] create directory before allocating socket
    Also, ":" are not allowed in mount specifications, remove them.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- pkg/netdevice/userVdpaInfo.go
+++ pkg/netdevice/userVdpaInfo.go
@@ -5,6 +5,8 @@ import (
 	"github.com/amorenoz/govdpa/pkg/uvdpa"
 	"github.com/intel/sriov-network-device-plugin/pkg/types"
 	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1beta1"
+	"os"
+	"strings"
 )
 
 const (
@@ -38,22 +40,11 @@ func (ip *UserInfoProvider) GetMounts(pciAddr string) []*pluginapi.Mount {
 	var vdpaMount pluginapi.Mount
 	mounts := make([]*pluginapi.Mount, 0)
 
-	if ip.vdpaType == "server" {
-		// If the interfaces is in server mode we can mount the socket file
-		path := vdpaSocketPath(pciAddr)
-		vdpaMount = pluginapi.Mount{
-			ContainerPath: path,
-			HostPath:      path,
-			ReadOnly:      false,
-		}
-	} else if ip.vdpaType == "client" {
-		// In client mode we have to mount the directory
-		path := vdpaSocketDir(pciAddr)
-		vdpaMount = pluginapi.Mount{
-			ContainerPath: path,
-			HostPath:      path,
-			ReadOnly:      false,
-		}
+	path := vdpaSocketDir(pciAddr)
+	vdpaMount = pluginapi.Mount{
+		ContainerPath: path,
+		HostPath:      path,
+		ReadOnly:      false,
 	}
 	mounts = append(mounts, &vdpaMount)
 	return mounts
@@ -66,14 +57,28 @@ func vdpaSocketPath(pciAddr string) string {
 
 // vdpaSocketPath returns the vdpa socket path of a device
 func vdpaSocketDir(pciAddr string) string {
-	return fmt.Sprintf("%s/%s", vdpaBasePath, pciAddr)
+	pciDir := pciAddr
+	pciDir = strings.Replace(pciDir, ":", "__", -1)
+	pciDir = strings.Replace(pciDir, ".", "_", -1)
+	return fmt.Sprintf("%s/%s", vdpaBasePath, pciDir)
 }
 
 func userVdpaAllocate(device string, vdpaType string) error {
+	sockDir := vdpaSocketDir(device)
+	if _, err := os.Stat(sockDir); !os.IsNotExist(err) {
+		if err := os.RemoveAll(sockDir); err != nil {
+			return err
+		}
+	}
+	if err := os.MkdirAll(sockDir, 0600); err != nil {
+		return err
+	}
+
 	u := uvdpa.NewVdpaClient(false)
 	if err := u.Init(); err != nil {
 		return err
 	}
+
 	return u.Create(uvdpa.VhostIface{
 		Device: device,
 		Socket: vdpaSocketPath(device),
