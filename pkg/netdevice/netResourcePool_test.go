3a1537f228aa391df9e6e64ec8d817e08a0a804f
Adrian Moreno <amorenoz@redhat.com>
2020-10-08 18:49:23 +0200

    pciNetDevice: Add Rdma Device InfoProvider
    Unlike DeviceSpecs coming from other drivers, RDMA DeviceSpecs are
    appended on Allocation time (in the netResourcePool getDeviceSpecs()).
    Apart from being sub-optimal, this opens the door for other logic being
    implemented in Allocation time (e.g: vhostDev).
    
    Implement the Rdma DeviceSpec injection using a DeviceInfoProvider thus
    cleaning the ResourcePool from any device-specific logic.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- pkg/netdevice/netResourcePool_test.go
+++ pkg/netdevice/netResourcePool_test.go
@@ -92,49 +92,5 @@ var _ = Describe("NetResourcePool", func() {
 				Expect(actual).To(ContainElement(fake2ds[0]))
 			})
 		})
-		Context("for RDMA devices", func() {
-			rc := &types.ResourceConfig{
-				ResourceName:   "fake",
-				ResourcePrefix: "fake",
-				SelectorObj: &types.NetDeviceSelectors{
-					IsRdma: true,
-				},
-			}
-			devs := map[string]*v1beta1.Device{}
-			rdma1 := &mocks.RdmaSpec{}
-			rdma2 := &mocks.RdmaSpec{}
-
-			// fake1 will have 2 RDMA device specs and 0 regular device specs
-			fake1 := &mocks.PciNetDevice{}
-			fake1ds := []*pluginapi.DeviceSpec{
-				&pluginapi.DeviceSpec{ContainerPath: "/fake/path", HostPath: "/dev/fake1a"},
-				&pluginapi.DeviceSpec{ContainerPath: "/fake/path", HostPath: "/dev/fake1b"},
-			}
-			rdma1.On("IsRdma").Return(true).On("GetRdmaDeviceSpec").Return(fake1ds)
-			fake1.On("GetRdmaSpec").Return(rdma1).
-				On("GetDeviceSpecs").Return(make([]*pluginapi.DeviceSpec, 0))
-
-			// fake2 will have 0 rdma device specs to trigger error msg
-			fake2 := &mocks.PciNetDevice{}
-			fake2ds := []*pluginapi.DeviceSpec{}
-			rdma2.On("IsRdma").Return(false).On("GetRdmaDeviceSpec").Return(fake2ds)
-			fake2.On("GetDeviceSpecs").Return(make([]*pluginapi.DeviceSpec, 0)).
-				On("GetRdmaSpec").Return(rdma2)
-
-			pcis := map[string]types.PciDevice{"fake1": fake1, "fake2": fake2}
-
-			rp := netdevice.NewNetResourcePool(rc, devs, pcis)
-
-			devIDs := []string{"fake1", "fake2"}
-
-			actual := rp.GetDeviceSpecs(devIDs)
-
-			It("should return valid slice of device specs", func() {
-				Expect(actual).ToNot(BeNil())
-				Expect(actual).To(HaveLen(2)) // fake1 => 2 rdma devices
-				Expect(actual).To(ContainElement(fake1ds[0]))
-				Expect(actual).To(ContainElement(fake1ds[1]))
-			})
-		})
 	})
 })
